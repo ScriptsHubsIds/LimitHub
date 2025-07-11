cd7908fb0b73080f2bce3e67670b59683a184c47
Lib <ybanezjosevincent.bshm.cpc@gmail.com>
2025-07-11 22:23:35 +0800

    Update LimitHub_Loader.lua

--- LimitHub_Loader.lua
+++ LimitHub_Loader.lua
@@ -1,2 +1,1 @@
 loadstring(game:HttpGet(('https://raw.githubusercontent.com/FakeModz/LimitHub/refs/heads/main/LimitHub_Luarmor_E.lua')))()
-loadstring(game:HttpGet("https://pastefy.app/7gU3W5x2/raw", true))()
